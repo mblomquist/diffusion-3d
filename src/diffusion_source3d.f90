9977ba6290e1b361ef040d38334d5ff05b3cea58
Matt Blomquist <blomquist.matt@gmail.com>
2018-02-06 15:30:49 -0800

    Update diffusion_source3d.f90

--- src/diffusion_source3d.f90
+++ src/diffusion_source3d.f90
@@ -1,156 +1,79 @@
-! diffusion_source2d
+! diffusion_source3d
 !
 ! Written by Matt Blomquist
-! Last Update: 2018-02-02 (YYYY-MM-DD)
+! Last Update: 2018-02-06 (YYYY-MM-DD)
 !
-! This program updates the sources term for a 2D diffusion problem.
+! This program updates the sources terms for a 3D diffusion problem.
 
-subroutine diffusion_source2d
+subroutine diffusion_source3d
 
   ! Include standard variable header
-  include "var2d.dec"
+  include "var3d.dec"
 
   ! Define internal variables
-  integer :: i, j
+  integer :: i, j, k
 
-  ! Run loop for internal nodes
-  do j = 2, n-1
-    do i = 2, m-1
+  ! Update source terms
+  do k = 1,l
+    do j = 1,n
+	  do i = 1,m
 
-      As(i,j) = k*A_y/dy
-      Aw(i,j) = k*A_x/dx
-      Ae(i,j) = k*A_x/dx
-      An(i,j) = k*A_y/dy
-      Ap(i,j) = As(i,j)+Aw(i,j)+Ae(i,j)+An(i,j)-Sp_t(i,j)
+	    ! Set default parameters
+	    Ab(i,j,k) = k*A_z/dz
+		As(i,j,k) = k*A_y/dy
+		Aw(i,j,k) = k*A_x/dx
+		Ae(i,j,k) = k*A_x/dx
+		An(i,j,k) = k*A_y/dy
+		At(i,j,k) = k*A_z/dz
+		
+		! Check bottom
+		if (k .eq. 1) then
+		  Ab(i,j,k) = 0
+		end if
 
-      b(i,j) = Sc_t(i,j)
+		! Check top
+		if (k .eq. l) then
+		  At(i,j,k) = 0
+		end if
 
-      x_initial(i+(j-1)*m) = T(i,j)
+		! Check south
+		if (j .eq. 1) then
+		  As(i,j,k) = 0
+		end if
 
-    end do
+		! Check north
+		if (j .eq. n) then
+		  An(i,j,k) = 0
+		end if
+		
+		! Check west
+		if (i .eq. 1) then
+		  Aw(i,j,k) = 0
+		end if
+
+		! Check east
+		if (i .eq. m) then
+		  Ae(i,j,k) = 0
+		end if
+
+		! Update Ap
+		Ap(i,j,k) = Ab(i,j,k)+As(i,j,k)+Aw(i,j,k)+Ae(i,j,k)+An(i,j,k)+At(i,j,k)-Sp_t(i,j,k)
+
+		! Update b
+		b(i,j,k) = Sc_t(i,j,k)
+
+	  end do
+	end do
   end do
-
-  ! Run loop for south and north edges
-  do j = 2,n-1
-
-    i = 1
-
-    As(i,j) = k*A_x/dx
-    Aw(i,j) = 0
-    Ae(i,j) = k*A_x/dx
-    An(i,j) = k*A_y/dy
-    Ap(i,j) = As(i,j)+Aw(i,j)+Ae(i,j)+An(i,j)-Sp_t(i,j)
-
-    b(i,j) = Sc_t(i,j)
-
-    x_initial(i+(j-1)*m) = T(i,j)
-
-    i = m
-
-    As(i,j) = k*A_y/dy
-    Aw(i,j) = k*A_x/dx
-    Ae(i,j) = 0
-    An(i,j) = k*A_x/dx
-    Ap(i,j) = As(i,j)+Aw(i,j)+Ae(i,j)+An(i,j)-Sp_t(i,j)
-
-    b(i,j) = Sc_t(i,j)
-
-    x_initial(i+(j-1)*m) = T(i,j)
-
-  end do
-
-  ! Run loop for west and east edges
-  do i = 2,m-1
-
-    j = 1
-
-    As(i,j) = 0
-    Aw(i,j) = k*A_x/dx
-    Ae(i,j) = k*A_x/dx
-    An(i,j) = k*A_y/dy
-    Ap(i,j) = As(i,j)+Aw(i,j)+Ae(i,j)+An(i,j)-Sp_t(i,j)
-
-    b(i,j) = Sc_t(i,j)
-
-    x_initial(i+(j-1)*m) = T(i,j)
-
-    j = n
-
-    As(i,j) = k*A_y/dy
-    Aw(i,j) = k*A_x/dx
-    Ae(i,j) = k*A_x/dx
-    An(i,j) = 0
-    Ap(i,j) = As(i,j)+Aw(i,j)+Ae(i,j)+An(i,j)-Sp_t(i,j)
-
-    b(i,j) = Sc_t(i,j)
-
-    x_initial(i+(j-1)*m) = T(i,j)
-
-  end do
-
-  ! Define corners
-  ! West - South
-  i = 1
-  j = 1
-
-  As(i,j) = 0
-  Aw(i,j) = 0
-  Ae(i,j) = k*A_x/dx
-  An(i,j) = k*A_y/dy
-  Ap(i,j) = As(i,j)+Aw(i,j)+Ae(i,j)+An(i,j)-Sp_t(i,j)
-
-  b(i,j) = Sc_t(i,j)
-
-  x_initial(i+(j-1)*m) = T(i,j)
-
-  ! West-North
-  i = 1
-  j = n
-
-  As(i,j) = k*A_y/dy
-  Aw(i,j) = 0
-  Ae(i,j) = k*A_x/dx
-  An(i,j) = 0
-  Ap(i,j) = As(i,j)+Aw(i,j)+Ae(i,j)+An(i,j)-Sp_t(i,j)
-
-  b(i,j) = Sc_t(i,j)
-
-  x_initial(i+(j-1)*m) = T(i,j)
-
-  ! East-South
-  i = m
-  j = 1
-
-  As(i,j) = 0
-  Aw(i,j) = k*A_y/dy
-  Ae(i,j) = 0
-  An(i,j) = k*A_y/dy
-  Ap(i,j) = As(i,j)+Aw(i,j)+Ae(i,j)+An(i,j)-Sp_t(i,j)
-
-  b(i,j) = Sc_t(i,j)
-
-  x_initial(i+(j-1)*m) = T(i,j)
-
-  ! East-North
-  i = m
-  j = n
-
-  As(i,j) = k*A_y/dy
-  Aw(i,j) = k*A_y/dy
-  Ae(i,j) = 0
-  An(i,j) = 0
-  Ap(i,j) = As(i,j)+Aw(i,j)+Ae(i,j)+An(i,j)-Sp_t(i,j)
-
-  b(i,j) = Sc_t(i,j)
-
-  x_initial(i+(j-1)*m) = T(i,j)
 
   ! Update A values for solver
+  Ab = -Ab
   As = -As
   Aw = -Aw
   Ae = -Ae
   An = -An
+  At = -At
 
   return
 
-end subroutine diffusion_source2d
+end subroutine diffusion_source3d
