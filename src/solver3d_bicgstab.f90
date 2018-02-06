a720c06212059d271517b1cab8d126a754f660c4
Matt Blomquist <blomquist.matt@gmail.com>
2018-02-06 15:30:07 -0800

    Update solver3d_bicgstab.f90

--- src/solver3d_bicgstab.f90
+++ src/solver3d_bicgstab.f90
@@ -1,7 +1,7 @@
-! solver3d_bicgstab_mcfb
+! solver3d_bicgstab
 !
 ! Written by Matt Blomquist
-! Last Update: 2018-01-31 (YYYY-MM-DD)
+! Last Update: 2018-02-06 (YYYY-MM-DD)
 !
 ! This program solves a three-dimensional finite volume discretization problem
 ! using the bi-conjugate gradients stabilized algorithm.
@@ -20,7 +20,7 @@
 !   maxit :: on exit, this value contains the number of iterations of the BiCGStab algorithm
 !   tol :: on exit, this value represents the normalized residual
 
-subroutine solver3d_bicgstab_mcfb(Ab, As_in, Aw, Ap_in, Ae, An, At, b, phi, m, n, l, tol, maxit)
+subroutine solver3d_bicgstab(Ab, As, Aw, Ap, Ae, An, At, b, phi, m, n, l, tol, maxit)
 
   ! Define implicit
   implicit none
@@ -32,14 +32,14 @@ subroutine solver3d_bicgstab_mcfb(Ab, As_in, Aw, Ap_in, Ae, An, At, b, phi, m, n
   integer, intent(in) :: m, n, l
   integer, intent(inout) :: maxit
   real(8), intent(inout) :: tol
-  real(8), dimension(m,n,l), intent(in) :: Ab, As_in, Aw, Ap_in, Ae, An, At, b
+  real(8), dimension(m,n,l), intent(in) :: Ab, As, Aw, Ap, Ae, An, At, b
   real(8), dimension(m,n,l), intent(inout) :: phi
 
   ! Define internal variables
   integer :: i, j, k, itr
   real(8), dimension(m*n*l,7) :: A_values
   integer, dimension(7) :: A_distance
-  real(8), dimension(m*n*l) :: r, r0, r1, x, p, Ap, As, s, Ax, b_values
+  real(8), dimension(m*n*l) :: r, r0, r1, x, p, Axp, Axs, s, Axx, b_values
   real(8) :: alpha, omega, beta, r_norm
 
 
@@ -52,9 +52,9 @@ subroutine solver3d_bicgstab_mcfb(Ab, As_in, Aw, Ap_in, Ae, An, At, b, phi, m, n
 
         ! Compress stiffness matrix values
         A_values(i+(j-1)*m+(k-1)*m*n,1) = Ab(i,j,k)
-        A_values(i+(j-1)*m+(k-1)*m*n,2) = As_in(i,j,k)
+        A_values(i+(j-1)*m+(k-1)*m*n,2) = As(i,j,k)
         A_values(i+(j-1)*m+(k-1)*m*n,3) = Aw(i,j,k)
-        A_values(i+(j-1)*m+(k-1)*m*n,4) = Ap_in(i,j,k)
+        A_values(i+(j-1)*m+(k-1)*m*n,4) = Ap(i,j,k)
         A_values(i+(j-1)*m+(k-1)*m*n,5) = Ae(i,j,k)
         A_values(i+(j-1)*m+(k-1)*m*n,6) = An(i,j,k)
         A_values(i+(j-1)*m+(k-1)*m*n,7) = At(i,j,k)
@@ -77,35 +77,35 @@ subroutine solver3d_bicgstab_mcfb(Ab, As_in, Aw, Ap_in, Ae, An, At, b, phi, m, n
     x = 1
   end if
 
-  call mkl_ddiagemv('N', m*n*l, A_values, m*n*l, A_distance, 7, x, Ax)
-  r = b_values - Ax
+  call mkl_ddiagemv('N', m*n*l, A_values, m*n*l, A_distance, 7, x, Axx)
+  r = b_values - Axx
   r0 = 1
   p = r
 
   do itr = 1,maxit
 
 
-    call mkl_ddiagemv('N', m*n*l, A_values, m*n*l, A_distance, 7, p, Ap)
-    alpha = ddot(m*n*l,r,1,r0,1)/ddot(m*n*l,Ap,1,r0,1)
+    call mkl_ddiagemv('N', m*n*l, A_values, m*n*l, A_distance, 7, p, Axp)
+    alpha = ddot(m*n*l,r,1,r0,1)/ddot(m*n*l,Axp,1,r0,1)
 
-    s = r - alpha*Ap
+    s = r - alpha*Axp
 
-    call mkl_ddiagemv('N', m*n*l, A_values, m*n*l, A_distance, 7, s, As)
-    omega = ddot(m*n*l,As,1,s,1) / ddot(m*n*l,As,1,As,1)
+    call mkl_ddiagemv('N', m*n*l, A_values, m*n*l, A_distance, 7, s, Axs)
+    omega = ddot(m*n*l,As,1,s,1) / ddot(m*n*l,Axs,1,As,1)
 
     x = x + alpha*p + omega*s
 
-    r1 = s - omega*As
+    r1 = s - omega*Axs
 
     beta = ddot(m*n*l,r1,1,r0,1) / ddot(m*n*l,r,1,r0,1) * alpha/omega
 
-    p = r1 + beta*(p-omega*Ap)
+    p = r1 + beta*(p-omega*Axp)
 
     r = r1
 
     ! Check convergence of BiCG
-    call mkl_ddiagemv('N', m*n*l, A_values, m*n*l, A_distance, 7, x, Ax)
-	r_norm = abs(dnrm2(m*n*l, b_values-Ax, 1))
+    call mkl_ddiagemv('N', m*n*l, A_values, m*n*l, A_distance, 7, x, Axx)
+	r_norm = abs(dnrm2(m*n*l, b_values-Axx, 1))
 
     if (r_norm < tol) then
         print *, 'BiCGStab Algorithm successfully converged!'
@@ -141,4 +141,4 @@ subroutine solver3d_bicgstab_mcfb(Ab, As_in, Aw, Ap_in, Ae, An, At, b, phi, m, n
 
   return
 
-end subroutine solver3d_bicgstab_mcfb
+end subroutine solver3d_bicgstab
