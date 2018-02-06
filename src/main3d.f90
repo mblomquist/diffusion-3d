1a87fd3765f2a5580058512bf41de7396549cfd7
Matt Blomquist <blomquist.matt@gmail.com>
2018-02-06 15:32:23 -0800

    Update main3d.f90

--- src/main3d.f90
+++ src/main3d.f90
@@ -1,27 +1,27 @@
-! Diffusion Solver
+! 3D Diffusion Solver
 !
 ! Written by Matt Blomquist
-! Last Update: 2018-02-02 (YYYY-MM-DD)
+! Last Update: 2018-02-06 (YYYY-MM-DD)
 !
-! This program solves two-dimensional diffusions problems using the
-! BiCG algorithm.
+! This program solves three-dimensional diffusions problems using the
+! BiCG algorithm and Line-by-Line TDMA.
 
-program main2d
+program main3d
 
   ! Include standard variable header
-  include "var2d.dec"
+  include "var3d.dec"
 
   ! Initialize boundary conditions
-  print *, "Initialize 2D Diffusion Problem"
-  call initialize2d
+  print *, "Initialize 3D Diffusion Problem"
+  call initialize3d
 
   ! Output problem setup datafile
   print *, "Output Setup Datafile"
-  call output_setup2d
+  call output_setup3d
 
   ! Update source terms
   print *, "Update source terms"
-  call diffusion_source2d
+  call diffusion_source3d
 
   ! Set residual vector
   residual = 0
@@ -35,7 +35,7 @@ program main2d
   ! Run BiCG solver
   print *, "Run BiCGStab Solver"
   call cpu_time(start_time)
-  call solver2d_bicgstab(As, Aw, Ap, Ae, An, b, T, m, n, tol, maxit)
+  call solver3d_bicgstab(Ab, As, Aw, Ap, Ae, An, At, b, T, m, n, l, tol, maxit)
   call cpu_time(finish_time)
 
   print *, "Total runtime:", finish_time-start_time, "seconds."
@@ -46,7 +46,7 @@ program main2d
   ! Run BiCG solver
   print *, "Run Line by Line TDMA Solver"
   call cpu_time(start_time)
-  call solver2d_tdma(As, Aw, Ap, Ae, An, b, T, m, n, tol, maxit)
+  call solver23_tdma(Ab, As, Aw, Ap, Ae, An, At, b, T, m, n, l, tol, maxit)
   call cpu_time(finish_time)
 
   print *, "Total runtime:", finish_time-start_time, "seconds."
@@ -54,6 +54,6 @@ program main2d
 
   ! Output results datafile
   print *, "Output Results datafile"
-  call output_results2d
+  call output_results3d
 
-end program main2d
+end program main3d
